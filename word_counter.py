d4f6dd66244651e7116513753625e92c60e56828
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2019-06-12 16:25:55 -0400

    workable counter

---
+++ word_counter.py
@@ -0,0 +1,23 @@
+# Prompt for file to run in program.
+fname = input('Enter File: ')
+handle = open(fname)
+
+# Creates dictionary.
+di = dict()
+for line in handle:
+    line = line.rstrip()
+    wds = line.split()
+    for w in wds:
+        # idiom: retrieve/create/update counter
+        di[w] = di.get(w, 0) + 1
+
+# Finding the most common word
+largest_count = -1
+word = None
+for k,v in di.items():
+    if v > largest_count:
+        largest_count = v
+        word = k
+
+print('Largest Word:', word)
+print('Word Count:', largest_count)
